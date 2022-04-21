acbd15fce7e7ad19224ef5740ecfb4c98dabddd7
LeDucHoang2002 <leduchoang.10022002@gmail.com>
2022-04-21 08:10:32 +0700

    cap nhap

--- src/Demo/DangNhap.java
+++ src/Demo/DangNhap.java
@@ -104,6 +104,11 @@ public class DangNhap extends javax.swing.JFrame {
         jButton1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jButton1.setForeground(new java.awt.Color(0, 153, 153));
         jButton1.setText("Đăng Ký");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
         jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 330, -1, -1));
 
         jButton2.setBackground(new java.awt.Color(204, 255, 204));
@@ -132,6 +137,13 @@ public class DangNhap extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        DangKy l = new DangKy();
+        l.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
